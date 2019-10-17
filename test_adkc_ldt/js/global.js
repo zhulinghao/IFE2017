a447e2483b95560e1bd25317c13432d56671e1f5
祝令昊 <linghao.zhu@e-melt.com>
2019-10-17 11:22:21 +0800

    RM ALERT

--- test_adkc_ldt/js/global.js
+++ test_adkc_ldt/js/global.js
@@ -13,42 +13,20 @@ function viewRest(){
 }
 window.onload = function() {
 	// loading
-	// var loadingBlock = document.querySelector('.loading-overlay')
-	// loadingBlock.style.display = 'none'
 	// s8 陀螺仪
 	var imgPosition = {
         start: [4,8],
         end: [14, 18]
     };
-    var imgCenterPosition = {
-        x: 15,
-        y: (imgPosition.end[1] - imgPosition.start[1]) / 2 + imgPosition.start[1]
-    };
-    var img = document.querySelector('#imgLdt');
     // 陀螺仪
 	var box = document.querySelectorAll('.box');
 	if (window.DeviceOrientationEvent) {
 		window.addEventListener('deviceorientation', function(event) {
-            alert(JSON.stringify(event))
 		    let {alpha, beta, gamma} = event;
 		    if (gamma*1.5 < 30 && gamma*1.5 > -30) {
 		    	box[0].style.webkitTransform = 'rotateY(' + gamma*1.5 + 'deg)';
 	            box[0].style.transform = 'rotateY(' + gamma*1.5 + '*1.5deg)';
 	            box[0].style.mozTransform = 'rotateY(' + gamma*1.5 + 'deg)';
-            }
-	        //左右旋转
-	        if(Math.abs(gamma)< 15){
-	            img.style.left = imgCenterPosition.x + gamma / 90 * 40 + 'rem';
-	            img.style.transform = 'perspective(200px)' + 'rotateY(' + Math.abs(gamma)/2 +'deg)';
-	        } else{
-	            img.style.transform = 'perspective(200px)' + 'rotateY(' + 15/2 +'deg)';
-	        }
-	        //上下旋转
-	        if(Math.abs(beta) < 15){
-	            img.style.top = imgCenterPosition.y + beta / 180 * 30 + 'rem';
-	            img.style.transform = 'perspective(200px)'+' rotateX(' + Math.abs(beta)/2 + 'deg)';
-	        }else{
-	            img.style.transform = 'perspective(200px)'+' rotateX(' + 15/2 + 'deg)';
             }
         }, false);
 	} else {
